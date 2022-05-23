c7208ef072085cdaf2e052e7b452ae4090650abb
Leosang-lx <1245977986@qq.com>
2022-05-23 09:27:23 +0800

    delete useless files

--- src/main/java/com/example/springproject/api/PurchaseRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springproject.api;
-
-import com.example.springproject.domain.PurchaseRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRecordRepository extends JpaRepository<PurchaseRecord, Long>{
-
-}
--- src/main/java/com/example/springproject/api/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springproject.api;
-
-import com.example.springproject.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findUserByUsername(String username);
-    public User findUserByUsernameAndPassword(String username, String password);
-}
